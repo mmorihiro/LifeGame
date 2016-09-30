0fe82e6b1ff74d38726bce769e6074531ed20300
MMorihiro <motakesi54@gmail.com>
2016-09-30 20:52:07 +0900

    Add files to use libgdx and java

---
+++ desktop/src/com/github/mmorihiro/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.github.mmorihiro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.mmorihiro.core.LifeGame;
+
+public class DesktopLauncher {
+    
+	public static void main (String[] arg) {
+        LwjglApplicationConfiguration config =
+                new LwjglApplicationConfiguration();
+        new LwjglApplication(new LifeGame(), config);
+    }
+}
