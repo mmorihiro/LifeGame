29b6825cf9cdca96b1e32f57ab26009490ecc9e6
MMorihiro <motakesi54@gmail.com>
2016-10-04 21:21:07 +0900

    Fixed #2 Displaying a button

--- core/src/main/com/github/mmorihiro/core/LifeGame.kt
+++ core/src/main/com/github/mmorihiro/core/LifeGame.kt
@@ -1,10 +1,35 @@
 package com.github.mmorihiro.core
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 
 class LifeGame : ApplicationAdapter() {
+    val stage by lazy {
+        Stage()
+    }
+
+    override fun create() {
+        Gdx.input.inputProcessor = stage
+        val table = Table()
+        val skin = Skin(Gdx.files.internal("uiskin.json"))
+        table.setFillParent(true)
+        val button = Button(skin)
+        table.add(button)
+        stage.addActor(table)
+    }
 
     override fun render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        stage.act(Gdx.graphics.deltaTime)
+        stage.draw()
+    }
 
+    override fun dispose() {
+        stage.dispose()
     }
 }
