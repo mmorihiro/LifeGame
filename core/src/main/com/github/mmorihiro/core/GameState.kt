513ba6d593d38b993d917b59df1688b99b8b99ab
MMorihiro <motakesi54@gmail.com>
2016-10-07 20:39:13 +0900

    #11 Add a label

---
+++ core/src/main/com/github/mmorihiro/core/GameState.kt
@@ -0,0 +1,11 @@
+package com.github.mmorihiro.core
+
+
+class GameState {
+    var generation = 0
+    get() = field
+    set(value) {
+        field = value
+    }
+    
+}
--- core/src/main/com/github/mmorihiro/core/LifeGame.kt
+++ core/src/main/com/github/mmorihiro/core/LifeGame.kt
@@ -4,11 +4,13 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 
 class LifeGame : ApplicationAdapter() {
+    val state = GameState()
     val stage by lazy {
         Stage()
     }
@@ -23,7 +25,15 @@ class LifeGame : ApplicationAdapter() {
             if (button.isPressed) button.setText("pushed")
             button.isPressed
         }
-        table.add(button)
+        
+        val generation = Label(state.generation.toString(), skin)
+        val centerX = Gdx.graphics.width / 2f
+        val centerY = Gdx.graphics.height / 2f
+        generation.setPosition(centerX, centerY + button.height)
+        button.setPosition(centerX, centerY)
+        table.addActor(generation)
+        table.addActor(button)
+        table.debug = true
         stage.addActor(table)
     }
 
