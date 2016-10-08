0c59c987b2a50c9762d4ee41f10b5e24fab2d996
MMorihiro <motakesi54@gmail.com>
2016-10-09 08:44:40 +0900

    Make window nonresizable  #1

--- desktop/src/com/github/mmorihiro/desktop/DesktopLauncher.java
+++ desktop/src/com/github/mmorihiro/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
         LwjglApplicationConfiguration config =
                 new LwjglApplicationConfiguration();
+        config.resizable = false;
         new LwjglApplication(new LifeGame(), config);
     }
 }
