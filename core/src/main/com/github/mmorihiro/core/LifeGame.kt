65210a4d65e1ba6b149ce2d5b8ac201986ac2bcd
MMorihiro <motakesi54@gmail.com>
2016-10-09 09:01:07 +0900

    #11 Increment generation

--- core/src/main/com/github/mmorihiro/core/LifeGame.kt
+++ core/src/main/com/github/mmorihiro/core/LifeGame.kt
@@ -3,51 +3,26 @@ package com.github.mmorihiro.core
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 
 class LifeGame : ApplicationAdapter() {
-    val state = GameState()
-    val stage by lazy {
-        Stage()
+   private val state by lazy {
+        GameState()
     }
 
-    override fun create() {
-        Gdx.input.inputProcessor = stage
-        val table = Table()
-        val skin = Skin(Gdx.files.internal("uiskin.json"))
-        table.setFillParent(true)
-        val button = TextButton("not pushed", skin)
-        button.addListener {
-            if (button.isPressed) button.setText("pushed")
-            button.isPressed
-        }
-        
-        val generation = Label(state.generation.toString(), skin)
-        val centerX = Gdx.graphics.width / 2f
-        val centerY = Gdx.graphics.height / 2f
-        generation.setPosition(centerX, centerY + button.height)
-        button.setPosition(centerX, centerY)
-        table.addActor(generation)
-        table.addActor(button)
-        table.debug = true
-        stage.addActor(table)
-    }
+    private fun stage() = state.stage
 
     override fun render() {
+        state.incrementGeneration()
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        stage.act(Gdx.graphics.deltaTime)
-        stage.draw()
+        stage().act(Gdx.graphics.deltaTime)
+        stage().draw()
     }
 
     override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width, height, true)
+        stage().viewport.update(width, height, true)
     }
 
     override fun dispose() {
-        stage.dispose()
+        stage().dispose()
     }
 }
