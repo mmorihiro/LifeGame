a090b789d73ac36b7fb770d9b0f5836e72518292
MMorihiro <motakesi54@gmail.com>
2016-10-22 16:29:39 +0900

    #17 Paint array elements which are true

--- core/src/main/com/github/mmorihiro/core/ArrayActor.kt
+++ core/src/main/com/github/mmorihiro/core/ArrayActor.kt
@@ -1,5 +1,6 @@
 package com.github.mmorihiro.core
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.scenes.scene2d.Actor
@@ -11,12 +12,19 @@ class ArrayActor : Actor() {
     private val renderer = ShapeRenderer()
     private val size = 10f
 
+    init {
+        array[0][0] = true
+        array[5][5] = true
+        array[10][10] = true
+    }
+
     override fun draw(batch: Batch?, parentAlpha: Float) {
         batch!!.end()
         renderer.projectionMatrix = batch.projectionMatrix
         renderer.transformMatrix = batch.transformMatrix
         renderer.translate(x, y, 0f)
-        renderer.begin(ShapeRenderer.ShapeType.Line)
+        renderer.begin(ShapeRenderer.ShapeType.Filled)
+        renderer.setAutoShapeType(true)
         drawArray()
         renderer.end()
         batch.begin()
@@ -25,6 +33,16 @@ class ArrayActor : Actor() {
     private fun drawArray() {
         val width = array[0].size * size
         val height = array.size * size
+        renderer.color = Color.GREEN
+        for (i in array.indices) {
+            for (j in array[0].indices) {
+                if (array[j][i]) {
+                    renderer.rect(size * j, size * i, size, size)
+                }
+            }
+        }
+        renderer.set(ShapeRenderer.ShapeType.Line)
+        renderer.color = Color.WHITE
         for (i in 0..arraySize) {
             renderer.line(0f, size * i, width, size * i)
             renderer.line(size * i, 0f, size * i, height)
