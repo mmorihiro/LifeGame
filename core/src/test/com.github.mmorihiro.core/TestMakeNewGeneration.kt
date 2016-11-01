5922560703222cf602398fd2c538ddddf5be1212
mmorihiro <motakesi54@gmail.com>
2016-11-01 10:35:07 +0900

    #29 Birth

--- core/src/test/com.github.mmorihiro.core/TestMakeNewGeneration.kt
+++ core/src/test/com.github.mmorihiro.core/TestMakeNewGeneration.kt
@@ -3,15 +3,24 @@ package com.github.mmorihiro.core
 import io.kotlintest.specs.WordSpec
 
 
-internal class TestMakeNewGeneration : WordSpec() {
+class TestMakeNewGeneration : WordSpec() {
     init {
+        val sut = MakeNewGeneration()
         "MakeNewGeneration.get" should {
             "return the same size array" {
-                val sut = MakeNewGeneration()
                 val expect = 10
                 val array = Array(expect) { BooleanArray(expect) { false } }
                 sut.get(array).size shouldBe expect
             }
+
+            """make new generation
+            when there are three living cell adjacent to one dead cell""" {
+                val array = arrayOf(
+                        booleanArrayOf(true, true),
+                        booleanArrayOf(true, false))
+                sut.get(array)[1][1] shouldBe true
+            }
         }
+
     }
 }
